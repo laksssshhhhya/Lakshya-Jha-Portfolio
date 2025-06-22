b0bc91c22ae40c43813819e8ade3b87fc66c6a2e
Lakshya Jha <lakshya2003jha@gmail.com>
2025-06-22 21:29:22 +0530

    commit

--- poetfolio/vite.config.js
+++ poetfolio/vite.config.js
@@ -5,7 +5,8 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  server: {
-    allowedHosts: ["41cf-27-60-4-26.ngrok-free.app"],
+  root: '.',
+  build: {
+    outDir: 'dist',
   },
 });
